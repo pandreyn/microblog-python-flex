3135970153ce6525fb1943ffc0ed7922b69f19ef
pandreyn <v-anpetk@microsoft.com>
2018-02-15 19:30:07 -0800

    Part 9: pagination

--- app/config.py
+++ app/config.py
@@ -12,5 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pandreyn@gmail.com']
-    FLASK_DEBUG = True
-    DEBUG = True
+    POSTS_PER_PAGE = 3
